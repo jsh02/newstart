001926421a83f4c1fc26ddc382fb2efa758bc8ab
정승환 <dandy249@naver.com>
2024-05-06 23:05:38 +0900

    1번째 커밋

---
+++ android/app/src/main/kotlin/com/example/newstart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.newstart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:async'; // Timer를 사용하기 위해 추가
+import 'package:flutter/material.dart';
+import 'login.dart'; // Import the login.dart file
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Navigator(
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(
+            builder: (context) => MainPage(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      // 3초 후 loginPage로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => loginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Image.asset(
+              'lib/image/logo.png', // 이미지 경로 확인 필요
+              width: 200,
+            ),
+            SizedBox(height: 60),
+            Text(
+              '앱 로딩 중...',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
